ae074ce23d4e42a44cee34041a6759c46161b8ea
Ryan Rasi <29680273+RyanRasi@users.noreply.github.com>
2023-05-14 22:00:27 +0100

    Random int generator and comments

--- main.go
+++ main.go
@@ -5,11 +5,14 @@ import (
 	"fmt"
 	"image/jpeg"
 	"io"
+	"io/ioutil"
 	"log"
+	"math/rand"
 	"net/http"
 	"os"
 	"strconv"
 	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/nfnt/resize"
@@ -17,40 +20,53 @@ import (
 
 // READ - All Items
 func getAny(w http.ResponseWriter, r *http.Request) {
+	// Set Headers
 	w.Header().Set("Content-Type", "image/jpeg")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 
-	img, err := os.Open("./assets/dog.jpg")
+	// Call random int function
+	picID := generateRandomInt()
+
+	// Open specfic image based on int
+	img, err := os.Open("./assets/" + strconv.Itoa(picID) + ".jpeg")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer img.Close()
+
+	// Display image onto webpage
 	io.Copy(w, img)
 }
 
 // READ - Single Item
 func getItem(w http.ResponseWriter, r *http.Request) {
+	// Set Headers
 	w.Header().Set("Content-Type", "image/jpeg")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
+
+	// Read params
 	allParams := mux.Vars(r)["dimensions"]
 	params := strings.Split(allParams, "x") // Get Params
 
 	width, height := params[0], params[1]
 
-	// open "test.jpg"
-	file, err := os.Open("./assets/dog3.jpeg")
+	// Call random int function
+	picID := generateRandomInt()
+
+	// Open specfic image based on int
+	file, err := os.Open("./assets/" + strconv.Itoa(picID) + ".jpeg")
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	// decode jpeg into image.Image
+	// Decode jpeg into image.Image
 	img, err := jpeg.Decode(file)
 	if err != nil {
 		log.Fatal(err)
 	}
 	file.Close()
 
-	// resize to width 1000 using Lanczos resampling
+	// Convert params to unsigned ints
 	w64, err := strconv.ParseUint(width, 10, 32)
 	if err != nil {
 		fmt.Println(err)
@@ -63,19 +79,50 @@ func getItem(w http.ResponseWriter, r *http.Request) {
 	}
 	ht := uint(h64)
 
-	// and preserve aspect ratio
+	// resize to width based on params using Lanczos resampling
 	newImage := resize.Resize(wd, ht, img, resize.Lanczos3)
 
 	var opt jpeg.Options
 	opt.Quality = 100
 
+	// Write image to memory
 	buff := bytes.NewBuffer(nil)
 	erro := jpeg.Encode(buff, newImage, &opt)
 	if erro != nil {
 		log.Fatal(erro)
 	}
 
+	// Display converted image onto webpage
 	io.Copy(w, buff)
+}
+
+func generateRandomInt() int {
+
+	// Folder path
+	folderPath := "./assets/"
+
+	// Read files in folder path
+	files, err := ioutil.ReadDir(folderPath)
+	if err != nil {
+		fmt.Println(err)
+		return 0
+	}
+
+	// Count all files in path
+	count := 0
+	for _, file := range files {
+		if !file.IsDir() {
+			count++
+		}
+	}
+	count--
+
+	// Generate number based on time seed
+	randSeed := rand.New(rand.NewSource(time.Now().UnixNano()))
+	number := randSeed.Intn(count) + 1
+
+	// Return the generated number
+	return number
 }
 
 func main() {
@@ -89,6 +136,7 @@ func main() {
 	router.HandleFunc("/any", getAny).Methods("GET")
 	router.HandleFunc("/get/{dimensions}", getItem).Methods("GET")
 
+	// Listener and logs
 	fmt.Println("PlaceDoggo")
 	fmt.Println("Web server started on port:", port)
 	http.Handle("/assets/", http.StripPrefix("/assets/", http.FileServer(http.Dir("./assets"))))
